6988a9f6973a121fb1e45d5bcdb5a85beeb7dd5a
albertoparente <albertoparentefh@gmail.com>
2021-10-06 23:28:55 -0300

    Create Layout and Title component, continuation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,9 +9,12 @@ export default function Layout(props: LayoutProps) {
     return (
         <div className={`
             flex flex-col w-2/3
-            bg-white text-gray-800
+            bg-white text-gray-800 rounded-md
         `}>
-        <Title></Title>
+        <Title>{props.title}</Title>
+        <div className="p-6">
+            {props.children}
+        </div>
         </div>        
     )
 }
