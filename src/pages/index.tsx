4788f1e6c14cffeaa9a9ec64d973e15721d475a1
albertoparente <albertoparentefh@gmail.com>
2021-10-10 22:47:49 -0300

    Creating table component, continuation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Layout from '../components/Layout'
 import Table from '../components/Table'
+import Client from '../core/Client'
 
 export default function Home() {
   const clients = [
-    new ('Alberto', '29', '1'),
-    new ('Juliana', '25', '2'),
-    new ('Julia', '1', '3'),
-    new ('Alice', '2', '4'), 
+    new Client('Alberto', 29, '1'),
+    new Client('Juliana', 25, '2'),
+    new Client('Julia', 1, '3'),
+    new Client('Alice', 2, '4'), 
   ]
   
   return (
