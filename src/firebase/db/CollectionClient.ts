09fae8b36681332e44bae7acffb179966bc837ed
albertoparente <albertoparentefh@gmail.com>
2021-10-18 22:27:31 -0300

    Creating client repository, continuation

--- src/firebase/db/CollectionClient.ts
+++ src/firebase/db/CollectionClient.ts
@@ -18,14 +18,26 @@ export default class CollectionClient implements ClientRepository {
     }
     
     async save(client: Client): Promise<Client> {
-        return null
+        if(client?.id) {
+            this.collection().doc(client.id).set(client)
+            return client
+        } else {
+            const docRef = await this.collection().add(client)
+            const doc = await docRef().get()
+            return doc.data()
+        }
     }
 
     async delete(client: Client): Promise<void> {
-        return null
+        return this.collection().doc(client.id).delete()
     }
 
     async getAll(): Promise<Client[]> {
-        return null
+       const query = await this.collection().get()
+       return query.docs.map(doc => doc.data()) ?? []
+    }
+
+    private collection() {
+        return firebase.firestore().collection('clients').withConverter(this.#conversor)
     }
 }
